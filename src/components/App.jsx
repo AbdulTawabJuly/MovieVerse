08befa5d6b88997c334796d8ea082788bab87816
AbdulTawabJuly <tawabmasood31july@gmail.com>
2023-07-25 23:04:14 +0500

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, Profile, NavBar } from './index';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-      <h2>Hello</h2>
-      <h3>C++</h3>
-      <h4>React</h4>
+    <div className="flex h-full">
+      <CssBaseline />
+      <NavBar />
+      <main className="py-4 px-4">
+        {/*Main tag is use to contain the main content of a document. It is just like a div*/}
+        <div className="h-70 font-bold" />
+        <Routes>
+          <Route exact path="/movies/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+
     </div>
   );
 }
-
 export default App;
